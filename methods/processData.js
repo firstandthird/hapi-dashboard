1e91461640bad3177bb65ae6166ae2f01f30899a
Greg Allen <greg@firstandthird.com>
2017-01-31 15:56:41 -0800

    output duration in log

--- methods/processData.js
+++ methods/processData.js
@@ -1,10 +1,13 @@
 module.exports = {
   method: (request, metric, options, done) => {
+    const start = new Date().getTime();
     metric.value.call(request, request.server, (err, data) => {
       if (err) {
         return request.server.log(['hapi-dashboard', 'error'], { message: err });
       }
-      request.server.log(['hapi-dashboard'], { message: `fetched data for ${metric.key}`, key: metric.key });
+      const end = new Date().getTime();
+      const diff = (end - start) / 1000;
+      request.server.log(['hapi-dashboard'], { message: `get data for ${metric.key}`, key: metric.key, duration: `${diff}s` });
       metric.data = data;
       done(null, metric, metric.ttl || options.ttl);
     });
