7f9202e8bcf50759510a68a73dd269cb3a9adde9
Chris <orthagonal1@gmail.com>
2017-04-06 10:41:16 -0500

    tests, test fixtures, test deps

---
+++ test/test.dashboard.js
@@ -0,0 +1,45 @@
+'use strict';
+const tap = require('tap');
+const setup = require('./setup.js');
+const fs = require('fs');
+let server;
+
+tap.beforeEach((allDone) => {
+  setup({}, (err, hapiServer) => {
+    if (err) {
+      return allDone(err);
+    }
+    server = hapiServer;
+    allDone();
+  });
+});
+
+tap.afterEach((done) => {
+  server.stop(done);
+});
+
+tap.test('can configure and call the dashboards menu endpoint', (t) => {
+  t.plan(2);
+  server.inject({
+    url: '/',
+    method: 'GET'
+  }, (response) => {
+    t.equal(response.statusCode, 200, 'dashboard endpoint returns HTTP OK');
+    const expectedOutput = fs.readFileSync('test/expectedOutputs/dashboard.html').toString();
+    t.equal(response.result, expectedOutput);
+    t.end();
+  });
+});
+
+tap.test('enables individual dashboards endpoint', (t) => {
+  t.plan(2);
+  server.inject({
+    url: '/users',
+    method: 'GET'
+  }, (response) => {
+    t.equal(response.statusCode, 200, 'dashboard endpoint returns HTTP OK');
+    const expectedOutput = fs.readFileSync('test/expectedOutputs/users.html').toString();
+    t.equal(response.result, expectedOutput);
+    t.end();
+  });
+});
